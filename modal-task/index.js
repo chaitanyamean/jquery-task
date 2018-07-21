347197dea82b96c6653643b15605c793d2ff33b9
ioskyc <Hack2Hire23@gmail.com>
2018-07-21 20:40:35 +0530

    initial push

---
+++ modal-task/index.js
@@ -0,0 +1,11 @@
+
+$(document).ready(() => {
+    $('#modal-btn').click(function () {
+        $('#login-form').modal({
+            // fadeDuration
+            fadeDuration: 1000
+        });
+
+    });
+
+});
